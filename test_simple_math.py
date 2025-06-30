2fa4b213f70457d4dfd6cb98b4562b86e5f9609d
Baptiste.S <soulier.baptiste@gmail.com>
2025-06-30 14:05:01 +0200

    Ajout de la fonction soustraction et de son test

--- test_simple_math.py
+++ test_simple_math.py
@@ -7,5 +7,10 @@ class TestSimpleMath(unittest.TestCase):
         self.assertEqual(SimpleMath.addition(-1, 1), 0)
         self.assertEqual(SimpleMath.addition(0, 0), 0)
 
+    def test_soustraction(self):
+        self.assertEqual(SimpleMath.soustraction(5, 2), 3)
+        self.assertEqual(SimpleMath.soustraction(10, 10), 0)
+        self.assertEqual(SimpleMath.soustraction(0, 5), -5)
+
 if __name__ == '__main__':
     unittest.main()
